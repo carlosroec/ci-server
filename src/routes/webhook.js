5abb8eebaf491b5ef4f292d03c02b21f7c7f8e05
Carlos G. Rodriguez <carlosro_ec@MacBook-Pro-de-Carlos.local>
2020-05-18 09:10:54 -0500

    Run the container on a push

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,12 +1,37 @@
+const { Docker } = require('node-docker-api');
 const GithubWebHook = require('express-github-webhook');
+
 const webhookHandler = GithubWebHook({ path: '/api/webhook', secret: 'jdhhhf465hsdjh' });
+const promisifyStream = stream => new Promise((resolve, reject) => {
+    stream.on('data', data => console.log(data.toString()))
+    stream.on('end', resolve)
+    stream.on('error', reject)
+});
 
 webhookHandler.on('*', function (event, repo, data) {
-    console.log(event);
-    console.log("----------");
-    console.log(repo);
-    console.log("=========");
-    console.log(data);
+    const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+    
+    // construimos el container en base a la imagen indicada
+    const containerName = `ci-${Math.floor(Math.random() * 1000)}`;
+    const container = await docker.container.create({
+        "Image": repo,
+        "name": containerName
+    });
+
+    // arrancamos el container y sus pruebas
+    await container.start();
+
+    const logsStream = await container.logs({
+        follow: true,
+        stdout: true,
+        stderr: true,
+        timestamps: true,
+    });
+
+    await promisifyStream(logsStream);
+
+    // eliminamos el container luego de ejecutar las pruebas
+    await container.delete({ force: true });
 });
 
 module.exports = webhookHandler;
